e877037387f287361611fc3f31a21072b14b8e8a
Andrew Adkins <adkinsar3@gmail.com>
2019-02-24 22:47:57 -0500

    changed greeting

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Wassup from Starbucks!
+            This is what a real workflow looks like!
           </p>
           <a
             className="App-link"
